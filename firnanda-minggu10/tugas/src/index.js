3a4dcdb1329465b44d3d3ba8b010e7c8fd8fb1b1
firnandakusuma <firnandapratama99@gmail.com>
2020-04-08 18:40:05 +0700

    Pengumpulan Tugas Minggu 10

--- firnanda-minggu10/tugas/src/index.js
+++ firnanda-minggu10/tugas/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
+import BlogPost from './container/BlogPost/BlogPost';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function HelloWorld() {
+//     return <p>ini adalah function compoent</p>
+// }
+
+// const HelloWorld = () => {
+//     return <p>ini adalah arrow function</p>
+// }
+
+// class Statefullcomponent extends React.Component {
+//     render() {
+//         return <p>ini adalah Statefull Component</p>
+//     }
+// }
+
+ReactDOM.render(<BlogPost />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
