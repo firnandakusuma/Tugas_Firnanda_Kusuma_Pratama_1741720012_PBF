7d62a7cdbe24bd754c5fb0772167f3f9f6f2c739
firnandakusuma <firnandapratama99@gmail.com>
2020-04-15 21:01:09 +0700

    Pengumpulan Tugas Minggu 11

---
+++ firnanda-minggu11/src/firebase.config.js
@@ -0,0 +1,11 @@
+export default {
+    apiKey: "AIzaSyAwMxDIexupyPxlJJWx6xHtXEjDg2xMqRo",
+    authDomain: "firnanda-firebase-react-6bed2.firebaseapp.com",
+    databaseURL: "https://firnanda-firebase-react-6bed2.firebaseio.com",
+    projectId: "firnanda-firebase-react-6bed2",
+    storageBucket: "firnanda-firebase-react-6bed2.appspot.com",
+    messagingSenderId: "365415768451",
+    appId: "1:365415768451:web:fad14c3eabfeb9e57556b6",
+    measurementId: "G-V0TQWC1E5P"
+  };
+
