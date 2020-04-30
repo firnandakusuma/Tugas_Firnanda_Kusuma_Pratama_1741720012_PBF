21bd0553e282c2dd38d0ea53aa76f89d8eaa3952
firnandakusuma <firnandapratama99@gmail.com>
2020-04-30 16:34:23 +0700

    Pengumpulan Tugas Minggu 13

---
+++ firnanda-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2yY7-7HAv-7-O6mfTqJRQwKQLAsZc1FU",
+    authDomain: "fir-login-5e7cf.firebaseapp.com",
+    databaseURL: "https://fir-login-5e7cf.firebaseio.com",
+    projectId: "fir-login-5e7cf",
+    storageBucket: "fir-login-5e7cf.appspot.com",
+    messagingSenderId: "349463083994",
+    appId: "1:349463083994:web:9f0ea1dc224be5d1189ee3",
+    measurementId: "G-CNGMYCDHVR"
+  };
+
+  export default firebaseConfig;
